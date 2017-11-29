b58a88563e707c62ffc5b1f05138b98231aa1a7a
Rodrigo Weigert <rodrigo.neorooker@gmail.com>
2017-11-29 00:14:02 -0200

    Switch e máquinas que se conectam só a ele.

---
+++ SW/root/switch-me.sh
@@ -0,0 +1,36 @@
+brctl addbr br0
+brctl addif br0 eth0
+brctl addif br0 eth1
+brctl addif br0 eth2
+brctl addif br0 eth3
+brctl addif br0 eth4
+brctl addif br0 eth5
+brctl addif br0 eth6
+brctl addif br0 eth7
+brctl addif br0 eth8
+brctl addif br0 eth9
+brctl addif br0 eth10
+brctl addif br0 eth11
+brctl addif br0 eth12
+brctl addif br0 eth13
+brctl addif br0 eth14
+brctl addif br0 eth15
+brctl addif br0 eth16
+ifconfig eth0 0.0.0.0
+ifconfig eth1 0.0.0.0
+ifconfig eth2 0.0.0.0
+ifconfig eth3 0.0.0.0
+ifconfig eth4 0.0.0.0
+ifconfig eth5 0.0.0.0
+ifconfig eth6 0.0.0.0
+ifconfig eth7 0.0.0.0
+ifconfig eth8 0.0.0.0
+ifconfig eth9 0.0.0.0
+ifconfig eth10 0.0.0.0
+ifconfig eth11 0.0.0.0
+ifconfig eth12 0.0.0.0
+ifconfig eth13 0.0.0.0
+ifconfig eth14 0.0.0.0
+ifconfig eth15 0.0.0.0
+ifconfig eth16 0.0.0.0
+ifconfig br0 192.168.1.10 netmask 255.255.255.0 broadcast 192.168.1.255
